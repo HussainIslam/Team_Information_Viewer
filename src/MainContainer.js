d118ff9e2bbbb7835d6d13ab90960f4c27c0d094
hussain <m.hussainul.islam@gmail.com>
2019-06-30 12:45:09 -0400

    altered MainComponent working

--- src/MainContainer.js
+++ src/MainContainer.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import NavBar from './NavBar';
 import SideBar from './SideBar';
-import ProjectPanel from './ProjectPanel';
-import TeamsPanel from './TeamsPanel';
-import EmployeesPanel from './EmployeesPanel'
 
 export default function(props){
   return(
@@ -13,18 +10,7 @@ export default function(props){
         <div className="row">
           <SideBar highlight="Overview" />
           <div className="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
-          <h1 className="page-header">Overview</h1>
-              <div className="row">
-                <div className="col-md-4">
-                  <ProjectPanel />
-                </div>
-                <div className="col-md-4">
-                  <TeamsPanel />
-                </div>
-                <div className="col-md-4">
-                  <EmployeesPanel />
-                </div>
-              </div>
+            {props.children}
           </div>
         </div>
 
--- src/Overview.js
+++ src/Overview.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import MainContainer from './MainContainer';
+import ProjectsPanel from './ProjectPanel';
+import TeamsPaPanel from './TeamsPanel';
+import EmployeesPanel from './EmployeesPanel';
 
 export default function(props){
   return(
-    <MainContainer highlight={props.highlight} />
+    <MainContainer highlight="Overview">
+      <h1 className="page-header">Overview</h1>
+      <div className="row">
+        <div className="col-md-4">
+          <ProjectsPanel />
+        </div>
+        <div className="col-md-4">
+          <TeamsPaPanel />
+        </div>
+        <div className="col-md-4">
+          <EmployeesPanel />
+        </div>
+      </div>
+    </MainContainer>
   );
 }
