ff7e8a523388fec62f485245d1c477793643acbb
hussain <m.hussainul.islam@gmail.com>
2019-06-30 01:52:09 -0400

    added SideBar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import NavBar from './NavBar';
+import Sidebar from './SideBar';
 
 class App extends Component {
   render() {
@@ -8,16 +9,7 @@ class App extends Component {
         <NavBar />
         <div className="container-fluid">
           <div className="row">
-            <div className="col-sm-3 col-md-2  sidebar">
-              <ul className="nav nav-sidebar">
-                <li className="active"><a href="/">Overview <span className="sr-only">(current)</span></a></li>
-              </ul>
-              <ul className="nav nav-sidebar">
-                <li><a href="/projects">Projects</a></li>
-                <li><a href="/teams">Teams</a></li>
-                <li><a href="/employees">Employees</a></li>
-              </ul>
-            </div>
+            <Sidebar highlight="Overview" />
             <div className=" col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
               <h1 className="page-header">Overview</h1>
               <div className="row">
