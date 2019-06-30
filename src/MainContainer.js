ddf068b92343c2e91ae1389f68f3861b79fa5384
hussain <m.hussainul.islam@gmail.com>
2019-06-30 12:47:28 -0400

    MainContainer fine touch

--- src/MainContainer.js
+++ src/MainContainer.js
@@ -8,7 +8,7 @@ export default function(props){
       <NavBar />
       <div className="container-fluid">
         <div className="row">
-          <SideBar highlight="Overview" />
+          <SideBar highlight={props.highlight} />
           <div className="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
             {props.children}
           </div>
