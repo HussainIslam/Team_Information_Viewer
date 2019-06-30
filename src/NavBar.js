1e632aca98b8c55acbce6f56f7aaa1f719074ed2
hussain <m.hussainul.islam@gmail.com>
2019-06-30 01:43:11 -0400

    added NavBar

---
+++ src/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function(){
+  return (
+      <div>
+        <nav className="navbar navbar-inverse navbar-fixed-top">
+            <div className="container-fluid">
+                <div className="navbar-header">
+                    <a className="navbar-brand" href="/">WEB422 - Project Portal</a>
+                </div>
+            </div>
+        </nav>
+      </div>
+  );
+}
